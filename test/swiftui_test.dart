4c2aec95355835240740cdfe92af0a790f288257
Luke Pighetti <lukepighetti@gmail.com>
2019-10-10 10:50:16 -0400

    generate project

---
+++ test/swiftui_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+// import 'package:swiftui/swiftui.dart';
+
+void main() {
+  test('has test coverage', () {
+    expect(true, isFalse);
+  });
+}
